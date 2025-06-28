8a4f587e8a93a79c58f27e3f66c4a7ce52650513
kalgo4444 <abuxyziabd@gmail.com>
2025-06-28 23:10:34 +0500

    add section others profiles pages

---
+++ assets/script/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const modalEL = document.querySelector('.modal')
+    const modalBtnEL = document.querySelector('.modalBtn')
+
+
+    const modal = () => modalEL.classList.remove('hidden')
+    setTimeout(modal, 2000)
+    modalBtnEL.onclick = () => modalEL.classList.add("hidden")
+})
